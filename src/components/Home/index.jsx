b56360817ef87a3f0e4f3bdf1e6d599811f96995
vijay-Ramaraju <vijayy2500@gmail.com>
2024-02-25 19:29:34 +0530

    Moved theTrendy design section moved to the top of the  Feature Design section

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -13,15 +13,14 @@ function Index() {
   return (
     <LayoutHomeFour>
       <Banner className="mb-[60px]" />
-      <BrandSection className="mb-[60px]" />
+      <BrandSection className="mb-[60px]" />      
+      <CampaignCountDown lastDate="2023-10-04 4:00:00" className="mb-[60px]" />
       <SectionStyleOneHmFour
         products={products.slice(16, 20)}
         sectionTitle="Trendy Design"
         seeMoreUrl="/all-products"
         className="trendyDesignSection new-products mb-[60px]"
       />
-      
-      <CampaignCountDown lastDate="2023-10-04 4:00:00" className="mb-[60px]" />
       <SectionStyleOneHmFour
         products={products.slice(20, 24)}
         sectionTitle="Feature Design"
