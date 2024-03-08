3b98f4676202e417e2a54b47031e1b77369431f0
vijay-Ramaraju <vijayy2500@gmail.com>
2024-03-08 16:08:10 +0530

    Removed Unitedstate selectbox from TopBar

--- src/components/Headers/TopBar.jsx
+++ src/components/Headers/TopBar.jsx
@@ -32,26 +32,7 @@ export default function TopBar({ className }) {
             </div>
             <div className="topbar-dropdowns sm:block hidden">
               <div className="flex space-x-6">
-                <div className="country-select flex space-x-1 items-center">
-                  <div>
-                    <img
-                      src={`${
-                        import.meta.env.VITE_PUBLIC_URL
-                      }/src/assets/images/country-logo-16x16.png`}
-                      width="16"
-                      height="16"
-                      alt="country logo"
-                      className="overflow-hidden rounded-full"
-                    />
-                  </div>
-                  <Selectbox
-                    className="w-fit"
-                    datas={["United State", "Bangladesh", "India"]}
-                  />
-                  <div>
-                    <Arrow className="fill-current qblack" />
-                  </div>
-                </div>
+                
                 <div className="currency-select flex space-x-1 items-center">
                   <Selectbox className="w-fit" datas={["USD", "BDT"]} />
                   <Arrow className="fill-current qblack" />
