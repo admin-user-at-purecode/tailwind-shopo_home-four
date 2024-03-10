ab0791c4677853712830753e172911073982ae75
vijay-Ramaraju <vijayy2500@gmail.com>
2024-03-10 13:26:08 +0530

    Changed the styles in the Feature Design section  (grid lg:grid-cols-6 sm:grid-cols-4 gap-2 grid-cols-2)

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -20,7 +20,7 @@ function Index() {
         className="new-products mb-[60px]"
       />
       <BrandSection className="mb-[60px]" />
-      <CampaignCountDown lastDate="2023-10-04 4:00:00" className="mb-[60px]" />
+      <CampaignCountDown lastDate="2023-10-04 4:00:00" className="mb-[60px] " />
       <SectionStyleOneHmFour
         products={products.slice(20, 24)}
         sectionTitle="Feature Design"
@@ -29,7 +29,7 @@ function Index() {
       />
       <ProductsAds
         ads={[`${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-3.png`]}
-        className="products-ads-section mb-[60px]"
+        className="products-ads-section h-96 mb-[60px]"
       />
       <SectionStyleThreeHmFour
         sectionTitle="New Arrival"
