268427ecf78c91933dd1f4ee181f429e4c67f544
vijay-Ramaraju <vijayy2500@gmail.com>
2024-03-11 17:52:59 +0530

    Removed 8 Card from Brandsection

--- src/components/Home/BrandSection.jsx
+++ src/components/Home/BrandSection.jsx
@@ -9,7 +9,7 @@ export default function BrandSection({ className, sectionTitle }) {
             </h1>
           </div>
         </div>
-        <div className="grid lg:grid-cols-6 sm:grid-cols-4 grid-cols-2">
+        <div className="grid lg:grid-cols-6 sm:grid-cols-4 grid-cols-2 gap:5">
           <div className="item">
             <div className="w-full h-[130px] bg-white border border-primarygray flex justify-center items-center">
               <img
@@ -50,86 +50,7 @@ export default function BrandSection({ className, sectionTitle }) {
               />
             </div>
           </div>
-          <div className="item">
-            <div className="w-full h-[130px] bg-white border border-primarygray flex justify-center items-center">
-              <img
-                src={`${
-                  import.meta.env.VITE_PUBLIC_URL
-                }/src/assets/images/brand-5.png`}
-                alt="logo"
-              />
-            </div>
-          </div>
-          <div className="item">
-            <div className="w-full h-[130px] bg-white border border-primarygray flex justify-center items-center">
-              <img
-                src={`${
-                  import.meta.env.VITE_PUBLIC_URL
-                }/src/assets/images/brand-6.png`}
-                alt="logo"
-              />
-            </div>
-          </div>
-          <div className="item">
-            <div className="w-full h-[130px] bg-white border border-primarygray flex justify-center items-center">
-              <img
-                src={`${
-                  import.meta.env.VITE_PUBLIC_URL
-                }/src/assets/images/brand-7.png`}
-                alt="logo"
-              />
-            </div>
-          </div>
-          <div className="item">
-            <div className="w-full h-[130px] bg-white border border-primarygray flex justify-center items-center">
-              <img
-                src={`${
-                  import.meta.env.VITE_PUBLIC_URL
-                }/src/assets/images/brand-8.png`}
-                alt="logo"
-              />
-            </div>
-          </div>
-          <div className="item">
-            <div className="w-full h-[130px] bg-white border border-primarygray flex justify-center items-center">
-              <img
-                src={`${
-                  import.meta.env.VITE_PUBLIC_URL
-                }/src/assets/images/brand-9.png`}
-                alt="logo"
-              />
-            </div>
-          </div>
-          <div className="item">
-            <div className="w-full h-[130px] bg-white border border-primarygray flex justify-center items-center">
-              <img
-                src={`${
-                  import.meta.env.VITE_PUBLIC_URL
-                }/src/assets/images/brand-10.png`}
-                alt="logo"
-              />
-            </div>
-          </div>
-          <div className="item">
-            <div className="w-full h-[130px] bg-white border border-primarygray flex justify-center items-center">
-              <img
-                src={`${
-                  import.meta.env.VITE_PUBLIC_URL
-                }/src/assets/images/brand-11.png`}
-                alt="logo"
-              />
-            </div>
-          </div>
-          <div className="item">
-            <div className="w-full h-[130px] bg-white border border-primarygray flex justify-center items-center">
-              <img
-                src={`${
-                  import.meta.env.VITE_PUBLIC_URL
-                }/src/assets/images/brand-12.png`}
-                alt="logo"
-              />
-            </div>
-          </div>
+          
         </div>
       </div>
     </div>
