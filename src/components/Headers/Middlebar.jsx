0e4ade1fc0f5a450815176107757b67da9e2f25f
vijay-Ramaraju <vijayy2500@gmail.com>
2024-03-11 19:19:51 +0530

    In the middleBar, Changed the Positions of ProfileButtons to Left of the middleBar

--- src/components/Headers/Middlebar.jsx
+++ src/components/Headers/Middlebar.jsx
@@ -7,20 +7,7 @@ export default function Middlebar({ className }) {
       <div className="container-x mx-auto h-full">
         <div className="relative h-full">
           <div className="flex justify-between items-center h-full">
-            <div>
-                <img
-                  width="152"
-                  height="36"
-                  src={`${
-                    import.meta.env.VITE_PUBLIC_URL
-                  }/src/assets/images/logo-4.svg`}
-                  alt="logo"
-                />
-            </div>
-            <div className="w-[517px] h-[44px]">
-              <SearchBox className="search-com" />
-            </div>
-            <div className="flex space-x-6 items-center">
+          <div className="flex space-x-6 items-center">
               <div className="compaire relative">
                   <span>
                     <Star />
@@ -54,6 +41,20 @@ export default function Middlebar({ className }) {
                   </span>
               </div>
             </div>
+            <div>
+                <img
+                  width="152"
+                  height="36"
+                  src={`${
+                    import.meta.env.VITE_PUBLIC_URL
+                  }/src/assets/images/logo-4.svg`}
+                  alt="logo"
+                />
+            </div>
+            <div className="w-[517px] h-[44px]">
+              <SearchBox className="search-com" />
+            </div>
+            
           </div>
         </div>
       </div>
