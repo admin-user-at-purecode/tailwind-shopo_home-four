b7c25224a78a041b4bb0cf2a81cf5c2ce6b84f6e
vijay-Ramaraju <vijayy2500@gmail.com>
2024-03-12 17:32:22 +0530

    Moved the Brand section to the Bottom of the newArrivalSection

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -19,7 +19,7 @@ function Index() {
         seeMoreUrl="/all-products"
         className="new-products mb-[60px]"
       />
-      <BrandSection className="mb-[60px]" />
+      
       <CampaignCountDown lastDate="2023-10-04 4:00:00" className="mb-[60px]" />
       <SectionStyleOneHmFour
         products={products.slice(20, 24)}
@@ -37,7 +37,7 @@ function Index() {
         products={products.slice(16, 28)}
         className="mb-[60px]"
       />
-
+<BrandSection className="mb-[60px]" />
       <ProductsAds
         sectionHeight="164"
         ads={[`${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-4.png`]}
