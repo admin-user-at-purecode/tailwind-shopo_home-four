66b8953d2968bd97cb82653f8653fd851500a8c9
vijay-Ramaraju <vijayy2500@gmail.com>
2024-03-12 18:07:06 +0530

    In Popular sales Component, Shuffeld the positions of the cardList

--- src/components/SectionStyleFour.jsx
+++ src/components/SectionStyleFour.jsx
@@ -14,7 +14,14 @@ export default function SectionStyleFour({
         <div className="products-section w-full">
           <div className="grid lg:grid-cols-3 grid-cols-1 xl:gap-[30px] lg:gap-5">
             <div className="item-col">
-              <DataIteration datas={products} startLength={0} endLength={4}>
+              <DataIteration datas={products} startLength={1} endLength={5}>
+                {({ datas }) => (
+                  <ProductCardRowStyleTwo key={datas.id} datas={datas} />
+                )}
+              </DataIteration>
+            </div>
+            <div className="item-col">
+              <DataIteration datas={products} startLength={2} endLength={6}>
                 {({ datas }) => (
                   <ProductCardRowStyleTwo key={datas.id} datas={datas} />
                 )}
@@ -22,13 +29,6 @@ export default function SectionStyleFour({
             </div>
             <div className="item-col">
               <DataIteration datas={products} startLength={4} endLength={8}>
-                {({ datas }) => (
-                  <ProductCardRowStyleTwo key={datas.id} datas={datas} />
-                )}
-              </DataIteration>
-            </div>
-            <div className="item-col">
-              <DataIteration datas={products} startLength={8} endLength={12}>
                 {({ datas }) => (
                   <ProductCardRowStyleTwo key={datas.id} datas={datas} />
                 )}
