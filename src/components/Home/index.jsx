f0aae7e5416316a65795869eb542d68be5ea76ea
vijay-Ramaraju <vijayy2500@gmail.com>
2024-03-13 12:40:40 +0530

    Moved featureDesignSection commponent to the bottom of the NavBar

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -12,6 +12,12 @@ function Index() {
   const { products } = datas;
   return (
     <LayoutHomeFour>
+      <SectionStyleOneHmFour
+        products={products.slice(20, 24)}
+        sectionTitle="Feature Design"
+        seeMoreUrl="/all-products"
+        className="new-products mb-[60px]"
+      />
       <Banner className="mb-[60px]" />
       <SectionStyleOneHmFour
         products={products.slice(16, 20)}
@@ -21,12 +27,7 @@ function Index() {
       />
       
       <CampaignCountDown lastDate="2023-10-04 4:00:00" className="mb-[60px]" />
-      <SectionStyleOneHmFour
-        products={products.slice(20, 24)}
-        sectionTitle="Feature Design"
-        seeMoreUrl="/all-products"
-        className="new-products mb-[60px]"
-      />
+      
       <ProductsAds
         ads={[`${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-3.png`]}
         className="products-ads-section mb-[60px]"
