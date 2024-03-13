82cdabe8c7d367b85315c9074c8a71e2f22f70d6
vijay-Ramaraju <vijayy2500@gmail.com>
2024-03-13 17:17:11 +0530

    Added Extra ProductsAdsone at the bottom of the popularSales

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -43,11 +43,17 @@ function Index() {
         ads={[`${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-4.png`]}
         className="products-ads-section mb-[60px]"
       />
+      
       <SectionStyleFour
         products={products.slice(16, 28)}
         sectionTitle="Popular Sales"
         seeMoreUrl="/all-products"
         className="mb-[60px]"
+      />
+      <ProductsAds
+        sectionHeight="164"
+        ads={[`${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-4.png`]}
+        className="products-ads-section mb-[60px]"
       />
     </LayoutHomeFour>
   );
