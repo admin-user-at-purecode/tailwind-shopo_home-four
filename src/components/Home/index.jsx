1727c002acfa349053499a65303e92de53660833
vijay-Ramaraju <vijayy2500@gmail.com>
2024-03-13 13:33:55 +0530

    Moved featureDesignSection to the bottom of the trendyDesignSection

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -19,14 +19,14 @@ function Index() {
         seeMoreUrl="/all-products"
         className="new-products mb-[60px]"
       />
-      
-      <CampaignCountDown lastDate="2023-10-04 4:00:00" className="mb-[60px]" />
       <SectionStyleOneHmFour
         products={products.slice(20, 24)}
         sectionTitle="Feature Design"
         seeMoreUrl="/all-products"
         className="new-products mb-[60px]"
       />
+      <CampaignCountDown lastDate="2023-10-04 4:00:00" className="mb-[60px]" />
+      
       <ProductsAds
         ads={[`${import.meta.env.VITE_PUBLIC_URL}/src/assets/images/ads-3.png`]}
         className="products-ads-section mb-[60px]"
