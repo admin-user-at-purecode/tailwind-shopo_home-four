7b46c1884940d5944d0cd3b3632259aee83ab1dd
vijay-Ramaraju <vijayy2500@gmail.com>
2024-03-14 11:51:54 +0530

    In the banner Component, Added Chip in the Cardlist

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -30,10 +30,14 @@ export default function Banner({ className }) {
                 <img src="/src/assets/images/banner-4.png" alt="thumb" />
                 <div className="absolute bottom-[32px] left-0 xl:w-[306px] sm:w-full w-[306px] bg-white py-6 px-[26px] bg-opacity-[0.92]">
                   <div className="mb-[15px]">
+                    <span className="px-2.5 py-[3px] mr-[5px] bg-qblack text-white text-xs font-semibold leading-5 uppercase rounded-full">
+                      bOYS STUYLE
+                    </span>
                     <span className="px-2.5 py-[3px] bg-qblack text-white text-xs font-semibold leading-5 uppercase rounded-full">
                       bOYS STUYLE
                     </span>
                   </div>
+                  
                   <p className="xl:text-[30px] sm:text-xl text-[30px] font-semibold xl:leading-10 sm:leading-normal leading-10 mb-[15px]">
                     Best Styles for all <br />
                     Boys
